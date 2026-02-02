9865b1edd174e05abf854c90768f73067b6d51c9
Jamie Bryson <jamieab98@gmail.com>
2026-02-01 22:08:08 -0500

    Added the function to reset the board and the ability for the checkwinner function to say who the winner is once the win conditions are set

--- Backend/Engines/tictactoe.py
+++ Backend/Engines/tictactoe.py
@@ -1,12 +1,17 @@
 class TicTacToe:
     def __init__(self):
-        self.board = ["","","","","","","","",""]
+        self.board = ["X","X","","","","","","",""]
         self.next_move = "X"
         self.active = True
     
     def checkwinner(self):
+        if self.next_move == "O":
+            winner = 'Player 1'
+        else:
+            winner = 'Player 2'
         if self.board[0] != "" and self.board[0] == self.board[1] == self.board[2]:
             self.active = False
+            print(f"The winner is {winner}")
         elif self.board[3] != "" and self.board[3] == self.board[4] == self.board[5]:
             self.active = False
         elif self.board[6] != "" and self.board[6] == self.board[7] == self.board[8]:
@@ -23,6 +28,9 @@ class TicTacToe:
             self.active = False
 
     def make_move(self, position):
+        if self.active == False:
+            print("Game is over")
+            return
         if self.board[position-1] != "":
             print("Somebody has alread gone there")
             return
@@ -33,5 +41,14 @@ class TicTacToe:
             self.next_move = "X"
         print(self.board)
         self.checkwinner()
+    
+    def reset(self):
+        self.board = ["","","","","","","","",""]
+        self.next_move = "X"
+        self.active = True
 
 t = TicTacToe()
+t.make_move(3)
+t.reset()
+t.make_move(1)
+t.make_move(2)
