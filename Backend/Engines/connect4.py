75968d5a5eeb5efde6cb6a5c11113e7ae958071c
Jamie Bryson <jamieab98@gmail.com>
2026-02-01 23:29:49 -0500

    Made a comment on how to procede

--- Backend/Engines/connect4.py
+++ Backend/Engines/connect4.py
@@ -4,7 +4,8 @@ class Connect4:
         self.next_move = "R"
     
     def make_move(self, col):
-        i = 0
+        i = 6
+        #Check the bottom of the column first. If that spot is occupied, move up. If it is not, place the coin there. If it reaches the top of the board, display the move was invalid and try again.
     
     def display_board(self):
         for row in self.board:
