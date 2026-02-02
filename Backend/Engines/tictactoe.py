9991d2faba5d45f5308d061635b584536a164076
Jamie Bryson <jamieab98@gmail.com>
2026-02-01 21:53:56 -0500

    Made all the game winning conditions

--- Backend/Engines/tictactoe.py
+++ Backend/Engines/tictactoe.py
@@ -3,16 +3,35 @@ class TicTacToe:
         self.board = ["","","","","","","","",""]
         self.next_move = "X"
         self.active = True
-        print(self.board)
+    
+    def checkwinner(self):
+        if self.board[0] != "" and self.board[0] == self.board[1] == self.board[2]:
+            self.active = False
+        elif self.board[3] != "" and self.board[3] == self.board[4] == self.board[5]:
+            self.active = False
+        elif self.board[6] != "" and self.board[6] == self.board[7] == self.board[8]:
+            self.active = False
+        elif self.board[0] != "" and self.board[0] == self.board[3] == self.board[6]:
+            self.active = False
+        elif self.board[1] != "" and self.board[1] == self.board[4] == self.board[7]:
+            self.active = False
+        elif self.board[2] != "" and self.board[2] == self.board[5] == self.board[8]:
+            self.active = False
+        elif self.board[0] != "" and self.board[0] == self.board[4] == self.board[8]:
+            self.active = False
+        elif self.board[2] != "" and self.board[2] == self.board[4] == self.board[6]:
+            self.active = False
 
     def make_move(self, position):
+        if self.board[position-1] != "":
+            print("Somebody has alread gone there")
+            return
         self.board[position-1] = self.next_move
         if self.next_move == "X":
             self.next_move = "O"
         else:
             self.next_move = "X"
         print(self.board)
+        self.checkwinner()
 
 t = TicTacToe()
-t.make_move(1)
-t.make_move(2)
